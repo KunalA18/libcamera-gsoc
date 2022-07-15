c0c6b7003f41379037751c94995eb035ee9c565d
Kunal Agarwal <kunalagarwal1072002@gmail.com>
2022-07-16 02:31:30 +0530

    imported dmabuf to EGL and bind eglimage to texture

--- src/libcamera/pipeline/simple/converter_gl.h
+++ src/libcamera/pipeline/simple/converter_gl.h
@@ -17,14 +17,19 @@ class FrameBuffer;
 class SimpleConverter
 {
 public:
-	int queueBuffer(FrameBuffer *input, FrameBuffer *output);
+	std::unique_ptr<FrameBuffer> queueBuffers(FrameBuffer *input, FrameBuffer *output);
 	void start();
 	void stop();
 	void exportBuffers(unsigned int count,
 			   std::vector<std::unique_ptr<FrameBuffer>> *buffers);
-	std::unique_ptr<FrameBuffer> createBuffer(unsigned int index);
+	std::pair<std::unique_ptr<FrameBuffer>, GlRenderTarget> createBuffer(unsigned int index);
 	int configure(const StreamConfiguration &inputCfg,
 		      const StreamConfiguration &outputCfg);
+	struct dmabuf_image {
+		GLuint texture;
+		EGLImageKHR image;
+	};
+	dmabuf_image import_dmabuf(int fd, Size pixelSize, libcamera::PixelFormat format);
 	struct converterFormat {
 		struct Plane {
 			uint32_t size = 0;
@@ -36,15 +41,40 @@ public:
 	};
 	converterFormat informat;
 	converterFormat outformat;
+	Shader shaderProgram;
+	Shader framebufferProgram;
+	std::vector<GlRenderTarget> outputBuffers;
+	// struct tex {
+	// 	int dmafd;
+	// 	dmabuf_image texture;
+	// };
 
 private:
-	std::map<libcamera::FrameBuffer *, std::unique_ptr<MappedFrameBuffer>> mappedBuffers_;
+	std::map<libcamera::FrameBuffer *, std::unique_ptr<MappedFrameBuffer>>
+		mappedBuffers_;
 	EGLDisplay dpy;
 	EGLSurface srf;
 	EGLContext ctx;
 	int fd;
 	struct gbm_device *gbm;
 	struct gbm_bo *bo;
+	unsigned int FBO;
+};
+
+class GlRenderTarget : public SimpleConverter
+{
+public:
+	struct dmabuf_image texture;
+
+	//private:
+	// This is never to be dereferenced. Only serves for comparison
+	const FrameBuffer *buffer;
+
+	GlRenderTarget(FrameBuffer *buffer_, struct dmabuf_image texture_)
+		: texture(texture_),
+		  buffer(buffer_)
+	{
+	}
 };
 
 } /* namespace libcamera */
--- src/libcamera/pipeline/simple/shaderClass.cpp
+++ src/libcamera/pipeline/simple/shaderClass.cpp
@@ -20,7 +20,7 @@ std::string get_file_contents(const char *filename)
 }
 
 // Constructor that build the Shader Program from 2 different shaders
-Shader::Shader(const char *vertexFile, const char *fragmentFile)
+void Shader::callShader(const char *vertexFile, const char *fragmentFile)
 {
 	// Read vertexFile and fragmentFile and store the strings
 	std::string vertexCode = get_file_contents(vertexFile);
